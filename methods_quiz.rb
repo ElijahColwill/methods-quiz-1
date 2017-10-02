003e04280f35341faefad28746a8272d7a3949b0
EljiahColwill <elijah.c035@gmail.com>
2017-10-02 10:36:18 -0600

    Implement has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,10 @@
   
 # TODO - write has_teen?
 
+def has_teen?(a, b, c)
+	return (a >= 13 and a <= 19) || (b >= 13 and b <= 19) || (c >= 13 and c <= 19)
+end
+
 # TODO - write not_string
 
 # TODO - write icy_hot?
